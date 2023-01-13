4a78e932679a68847fc55d0a191238366c18528e
qkrtjdwo5662 <qkrtjdwo5662@naver.com>
2023-01-13 17:33:20 +0900

    N_1010
    combination

--- BaekJoon/src/N_1010.java
+++ BaekJoon/src/N_1010.java
@@ -4,6 +4,7 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class N_1010 {
+    static int dp[][] = new int[30][30];
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -11,7 +12,19 @@ public class N_1010 {
         int n = Integer.parseInt(st.nextToken());
 
         for(int i=0; i<n; i++){
-             
+             st = new StringTokenizer(br.readLine());
+             int left = Integer.parseInt(st.nextToken());
+             int right = Integer.parseInt(st.nextToken());
+             System.out.println(combination(right,left));
+        }
+    }
+    static int combination(int n, int r){
+        if(dp[n][r]>0){
+            return dp[n][r];
+        } else if (n==r || r==0) {
+            return dp[n][r]=1;
+        }else{
+            return dp[n][r] = combination(n-1,r-1) + combination(n-1,r);
         }
     }
 }
