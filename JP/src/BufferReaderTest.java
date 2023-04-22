376507d406ddbb30d737e71ef840bbc6bd50d588
Seong-Jae Park <71877347+qkrtjdwo5662@users.noreply.github.com>
2023-04-22 23:36:56 +0900

    Update BufferReaderTest.java

--- JP/src/BufferReaderTest.java
+++ JP/src/BufferReaderTest.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
-
+//a
 public class BufferReaderTest{
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
