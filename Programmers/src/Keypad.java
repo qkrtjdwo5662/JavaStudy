d0e836667680429a06eaa0bd0a0d8db6b51e4e74
qkrtjdwo5662 <qkrtjdwo5662@naver.com>
2023-05-07 23:56:15 +0900

    Update Keypad.java

--- Programmers/src/Keypad.java
+++ Programmers/src/Keypad.java
@@ -9,17 +9,17 @@ public class Keypad {
         int lTemp =10;
         int RTemp =12;
         for (int i = 0; i < numbers.length; i++) {
-            if(numbers[i]==1 || numbers[i]==4 || numbers[i]==7 ){
+            if(numbers[i]==1 || numbers[i]==4 || numbers[i]==7 ){ // 1, 4, 7 -> L
                 sb.append("L");
                 lTemp = numbers[i];
-            } else if (numbers[i]==3 || numbers[i]==6 || numbers[i]==9) {
+            } else if (numbers[i]==3 || numbers[i]==6 || numbers[i]==9) { // 3, 6, 9 -> R
                 sb.append("R");
                 lTemp = numbers[i];
-            }else{
+            }else{ // 2, 5, 8, 0 -> l,r temp와의 거리비교
                 if(numbers[i]!=0){ // 2, 5, 8
                     if()
                 } else{ // 0
-                    
+
                 }
 
             }
