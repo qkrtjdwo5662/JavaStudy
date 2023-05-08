bceaa902ac5a3d74207ee21914facfd477f3b6ad
qkrtjdwo5662 <qkrtjdwo5662@naver.com>
2023-05-08 23:57:47 +0900

    keypad

--- Programmers/src/Keypad.java
+++ Programmers/src/Keypad.java
@@ -7,19 +7,48 @@ public class Keypad {
     static public String solution(int[] numbers, String hand){
         StringBuilder sb= new StringBuilder();
         int lTemp =10;
-        int RTemp =12;
+        int rTemp =12;
         for (int i = 0; i < numbers.length; i++) {
             if(numbers[i]==1 || numbers[i]==4 || numbers[i]==7 ){ // 1, 4, 7 -> L
                 sb.append("L");
                 lTemp = numbers[i];
             } else if (numbers[i]==3 || numbers[i]==6 || numbers[i]==9) { // 3, 6, 9 -> R
                 sb.append("R");
-                lTemp = numbers[i];
+                rTemp = numbers[i];
             }else{ // 2, 5, 8, 0 -> l,r temp와의 거리비교
                 if(numbers[i]!=0){ // 2, 5, 8
-                    if()
-                } else{ // 0
-
+                    int mL = Math.abs(numbers[i]-lTemp);
+                    int mR = Math.abs(numbers[i]-rTemp);
+                    int dL = mL%3 + mL/3;
+                    int dR = mR%3 + mR/3;
+                    if(dL < dR) {
+                        sb.append('L');
+                        lTemp = numbers[i];
+                    }else if(dL > dR) {
+                        sb.append('R');
+                        rTemp = numbers[i];
+                    } else {
+                        sb.append(hand.equals("right") ? "R" : "L");
+                        if(hand.equals("right")) rTemp = numbers[i];
+                        else lTemp = numbers[i];
+                    }
+                }else{ // 0
+                    numbers[i] = 11;
+                    int mL = Math.abs(numbers[i]-lTemp);
+                    int mR = Math.abs(numbers[i]-rTemp);
+                    int dL = mL%3 + mL/3;
+                    int dR = mR%3 + mR/3;
+                    if(dL < dR) {
+                        sb.append('L');
+                        lTemp = numbers[i];
+                    }else if(dL > dR) {
+                        sb.append('R');
+                        rTemp = numbers[i];
+                    } else {
+                        sb.append(hand.equals("right") ? "R" : "L");
+                        if(hand.equals("right")) rTemp = numbers[i];
+                        else lTemp = numbers[i];
+                    }
                 }
 
             }
