77cd760cc05e830bd78cd10516df03c09e5b1bac
qkrtjdwo5662 <qkrtjdwo5662@naver.com>
2023-05-27 23:58:05 +0900

    .

--- BaekJoon/src/N_3020.java
+++ BaekJoon/src/N_3020.java
@@ -5,6 +5,7 @@ import java.util.StringTokenizer;
 
 public class N_3020 {
     public static void main(String[] args) throws IOException {
+        long start = System.currentTimeMillis();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
@@ -19,19 +20,15 @@ public class N_3020 {
             if(i%2==0){
                 for (int j = board.length-1; j > board.length-1-obstacle; j--) {
                     board[j][i] = 1;
+                    board[j][board[j].length-1] = board[j][board[j].length-1] +1;
                 }
             }else {
                 for (int j = 0; j < obstacle; j++) {
                     board[j][i] = 1;
+                    board[j][board[j].length-1] = board[j][board[j].length-1] +1;
                 }
             }
-
         }
-//        for (int i = 0; i < board.length; i++){
-//            for (int j = 0; j < board[i].length-1; j++) {
-//                if(board[i][j]!=0) board[i][board[i].length-1] = board[i][board[i].length-1] + board[i][j];
-//            }
-//        }
 
         for (int i = 0; i < board.length; i++){
            min = Math.min(min, board[i][board[i].length-1]);
@@ -42,5 +39,8 @@ public class N_3020 {
         }
 
         System.out.println(min+" "+count);
+
+        long end = System.currentTimeMillis();
+        System.out.println((end - start)/1000.0);
     }
 }
