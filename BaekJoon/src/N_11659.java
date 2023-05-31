1ac0bb3f86a4ccfcf3135b0e4aecead58095462a
qkrtjdwo5662 <qkrtjdwo5662@naver.com>
2023-05-31 20:03:48 +0900

    cumulative sum

--- BaekJoon/src/N_11659.java
+++ BaekJoon/src/N_11659.java
@@ -1,23 +1,30 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-public class N_11659 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int M = sc.nextInt();
+class N_11659{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
 
-        int array[] = new int[N+1];
+        int [] arr = new int[n+1];
 
-        array[0] = 0;
-        for(int i=1; i<array.length; i++){
-            array[i] = array[i-1]+ sc.nextInt();
+        arr[0] = 0;
+        st = new StringTokenizer(br.readLine());
+        for (int i = 1; i < n+1; i++) {
+            arr[i] = arr[i-1] + Integer.parseInt(st.nextToken());
         }
-        for(int i=0; i<M; i++){
-            int start = sc.nextInt();
-            int end= sc.nextInt();
-            System.out.println(array[end]-array[start-1]);
+
+        for(int i=0; i<m; i++){
+            st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            sb.append(arr[end]-arr[start-1] + "\n");
         }
-
-
+        System.out.print(sb);
     }
 }
