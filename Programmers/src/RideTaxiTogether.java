587a26c9c2faed58e08f0f50a236a0aef28c17d5
qkrtjdwo5662 <qkrtjdwo5662@naver.com>
2023-08-12 22:25:03 +0900

    택시 합승 -> 다익스트라 X

--- Programmers/src/RideTaxiTogether.java
+++ Programmers/src/RideTaxiTogether.java
@@ -3,24 +3,24 @@ import java.util.Arrays;
 import java.util.PriorityQueue;
 
 public class RideTaxiTogether {
-    public static int n = 6; // 지점 개수
-    public static int s = 4; // 출발 지점
-    public static int a = 6; // a 도착 지점
-    public static int b = 2; // b 도착 지점
-    public static int[][] fares = {{4,1,10}, {3,5,24}, {5,6,2}, {3,1,41}, {5,1,24}, {4, 6, 50}, {2, 4, 66}, {2, 3, 22}, {1, 6, 25}};
+    public static int n = 7; // 지점 개수
+    public static int s = 3; // 출발 지점
+    public static int a = 4; // a 도착 지점
+    public static int b = 1; // b 도착 지점
+    public static int[][] fares = {{5, 7, 9}, {4, 6, 4}, {3, 6, 1}, {3, 2, 3}, {2, 1, 6}};
+
+    public static int INF = 100_000_000;
+    public static ArrayList<int[]>[] adjList;
     public static void main(String[] args) {
         System.out.println(solution(n, s, a, b, fares));
     }
 
     public static int solution(int n, int s, int a, int b, int[][] fares){
-        int answer = 0;
+        int answer = INF;
 
         // a와 b 지점은 같지 않음
 
-        // s -> a ,  s -> b
-        // s -> a -> b or s -> b -> a
-
-        ArrayList<int[]>[] adjList = new ArrayList[n+1];
+        adjList = new ArrayList[n+1];
         for (int i = 1; i <= n ; i++) {
             adjList[i] = new ArrayList<>();
         }
@@ -33,109 +33,40 @@ public class RideTaxiTogether {
             adjList[u].add(new int[]{v,w});
             adjList[v].add(new int[]{u,w});
         }
-        int each = 0;
-        boolean[] visitedA = new boolean[n+1];
-        boolean[] visitedB = new boolean[n+1];
+        // s-> i -> a, s -> i -> b
 
-        int INF = 987654321;
-        int[] distA = new int[n+1]; // 거리 계산
-        int[] distB = new int[n+1]; // 거리 계산
+        for (int i = 1; i <= n; i++) {
+            answer = Math.min(answer, dijkstra(s, i, n) + dijkstra(i, a, n) + dijkstra(i, b, n));
+        }
 
+        return answer;
+    }
+    public static int dijkstra(int start, int target, int n){
+        boolean[] visited = new boolean[n+1];
+        int[] dist = new int[n+1];
         PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2) -> {
             return Integer.compare(o1[1], o2[1]);
         });
 
-        Arrays.fill(distA, INF);
-        Arrays.fill(distB, INF);
-
-        distA[s] = 0;
-        distB[s] = 0;
-
-        pq.add(new int[]{s, 0});
-        while(!pq.isEmpty()){
-            int[] now = pq.poll();
-
-            if(visitedA[now[0]]) continue;
-            visitedA[now[0]] = true;
-
-            for (int i = 0; i < adjList[now[0]].size(); i++) {
-                int[] next = adjList[now[0]].get(i);
-
-                if(distA[next[0]] > distA[now[0]] + next[1]){
-                    distA[next[0]] = distA[now[0]] + next[1];
-                    pq.add(new int[]{next[0], distA[next[0]]});
-                }
-            }
-        }
-
-        pq.add(new int[]{s, 0});
-        while(!pq.isEmpty()){
-            int[] now = pq.poll();
-
-            if(visitedB[now[0]]) continue;
-            visitedB[now[0]] = true;
-
-            for (int i = 0; i < adjList[now[0]].size(); i++) {
-                int[] next = adjList[now[0]].get(i);
-
-                if(distB[next[0]] > distB[now[0]] + next[1]){
-                    distB[next[0]] = distB[now[0]] + next[1];
-                    pq.add(new int[]{next[0], distB[next[0]]});
-                }
-            }
-        }
-        each = distA[a] + distB[b];
-
-        // s -> a -> b
-        int together = 0;
-        boolean[] visited = new boolean[n+1];
-        int[] distMixFirst = new int[n+1];
-        int[] distMixSecond = new int[n+1];
-
-        Arrays.fill(distMixFirst, INF);
-        distMixFirst[s] = 0;
-        pq.add(new int[]{s, 0});
+        Arrays.fill(dist, INF);
+        dist[start] = 0;
+        pq.add(new int[]{start,0});
 
         while(!pq.isEmpty()){
             int[] now = pq.poll();
-
             if(visited[now[0]]) continue;
             visited[now[0]] = true;
 
             for (int i = 0; i < adjList[now[0]].size(); i++) {
                 int[] next = adjList[now[0]].get(i);
-
-                if(distMixFirst[next[0]] > distMixFirst[now[0]] + next[1]){
-                    distMixFirst[next[0]] = distMixFirst[now[0]] + next[1];
-                    pq.add(new int[]{next[0], distMixFirst[next[0]]});
+                if(dist[next[0]] > dist[now[0]] + next[1]){
+                    dist[next[0]] = dist[now[0]] + next[1];
+                    pq.add(new int[]{next[0], dist[next[0]]});
                 }
+
             }
         }
 
-        visited = new boolean[n+1];
-        Arrays.fill(distMixSecond, INF);
-        distMixSecond[a] = 0;
-        pq.add(new int[]{a, 0});
-
-        while(!pq.isEmpty()){
-            int[] now = pq.poll();
-
-            if(visited[now[0]]) continue;
-            visited[now[0]] = true;
-
-            for (int i = 0; i < adjList[now[0]].size(); i++) {
-                int[] next = adjList[now[0]].get(i);
-
-                if(distMixSecond[next[0]] > distMixSecond[now[0]] + next[1]){
-                    distMixSecond[next[0]] = distMixSecond[now[0]] + next[1];
-                    pq.add(new int[]{next[0], distMixSecond[next[0]]});
-                }
-            }
-        }
-
-        together = distMixFirst[a] + distMixSecond[b];
-
-        answer = Math.min(each, together);
-        return answer;
+        return dist[target];
     }
 }
