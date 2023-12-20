d0b148e199736c8b1e59f3b65621e298d7290915
qkrtjdwo5662 <qkrtjdwo5662@naver.com>
2023-12-20 20:42:08 +0900

    h2 db connection, jpa, jpql

--- JPA/src/main/java/hellojpa/JpaMain.java
+++ JPA/src/main/java/hellojpa/JpaMain.java
@@ -2,7 +2,9 @@ package hellojpa;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -11,7 +13,37 @@ public class JpaMain {
         EntityManager em = emf.createEntityManager();
         // code
 
-        em.close();
+        EntityTransaction ts = em.getTransaction();
+        ts.begin();
+
+        try{
+            // create
+//            Member member = new Member();
+//            member.setId(2L);
+//            member.setName("HelloB");
+//            em.persist(member);
+
+            // update
+//            Member findMember = em.find(Member.class, 1L);
+//            findMember.setName("helloJPA");
+
+            // delete
+//            Member findMember = em.find(Member.class, 1L);
+//            em.remove(findMember);
+
+            List<Member> result = em.createQuery("select m from Member as m", Member.class)
+                            .getResultList();
+
+            for(Member member: result){
+                System.out.println(member.getName());
+            }
+
+            ts.commit();
+        }catch(Exception e){
+            ts.rollback();
+        }finally {
+            em.close();
+        }
 
         emf.close();
     }
