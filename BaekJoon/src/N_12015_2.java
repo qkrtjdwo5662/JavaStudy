564a16dcad14909aee4bb22726f321e3125bde18
qkrtjdwo5662 <qkrtjdwo5662@naver.com>
2024-08-21 23:48:54 +0900

    feat: boj_12015

---
+++ BaekJoon/src/N_12015_2.java
@@ -0,0 +1,53 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class N_12015_2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(st.nextToken());
+        int[] arr = new int[n];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int[] lis = new int[n];
+
+        lis[0] = arr[0];
+        int max = 1;
+
+        for (int i = 1; i < n; i++) {
+            int key = arr[i];
+
+            if(lis[max - 1] < key){
+                max ++;
+                lis[max -1] = key;
+
+            }else{
+                int low = 0;
+                int high = max;
+                while(low < high){
+                    int mid = (low + high) /2 ;
+
+                    if(lis[mid] < key){
+                        low = mid + 1;
+                    }else{
+                        high = mid;
+                    }
+                }
+
+                lis[low] = key;
+
+            }
+        }
+        sb.append(max).append("\n");
+        System.out.println(max);
+
+    }
+}
