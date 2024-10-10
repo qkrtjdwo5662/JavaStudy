d2a9ceae05be0add3fbd8543711267af1d5242d5
qkrtjdwo5662 <qkrtjdwo5662@naver.com>
2024-10-10 22:55:38 +0900

    feat: boj_20115

---
+++ BaekJoon/src/N_20115.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class N_20115 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(st.nextToken());
+        int[] arr = new int[n];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+        
+        double max = arr[n - 1];
+        for (int i = 0; i < n - 1; i++) {
+            max += (double)arr[i]/2;
+        }
+
+        sb.append(max).append("\n");
+        System.out.println(sb);
+    }
+}
