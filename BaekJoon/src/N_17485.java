d6e097910c0325b61507cf70097cf3f2a0a08a3f
qkrtjdwo5662 <qkrtjdwo5662@naver.com>
2025-03-28 18:23:15 +0900

    feat: boj_17485

--- BaekJoon/src/N_17485.java
+++ BaekJoon/src/N_17485.java
@@ -1,15 +1,14 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class N_17485 {
     static int n;
     static int m;
     static int[][] arr;
-    static int answer;
-    static int[] rx = {-1, 0, 1};
-    static int[] ry = {1, 1, 1};
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -27,21 +26,8 @@ public class N_17485 {
                 arr[i][j] = num;
             }
         }
+
         int[][][] dp = new int[n][m][3];
-
-//        for (int i = 0; i < n; i++) {
-//            for (int j = 0; j <m ; j++) {
-//                for (int k = 0; k < 3; k++) {
-//                    dp[i][j][k] = Integer.MAX_VALUE;
-//                }
-//            }
-//        }
-
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < 3; j++) {
-                dp[0][i][j] = arr[0][i];
-            }
-        }
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
@@ -51,28 +37,46 @@ public class N_17485 {
             }
         }
 
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < 3; j++) {
+                dp[0][i][j] = arr[0][i];
+            }
+        }
+
+        // 0 : 직진
+        // 1 : 왼쪽
+        // 2 : 오른쪽
+
         for (int i = 1; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                if(isArea(j + rx[0])){
-
+                if(j == 0){
+                    dp[i][j][1] = Math.min(dp[i - 1][j][0], dp[i - 1][j + 1][2]) + arr[i][j];
+                    dp[i][j][0] = dp[i - 1][j + 1][2] + arr[i][j];
+                    continue;
+                }else if(j == m - 1){
+                    dp[i][j][2] = Math.min(dp[i - 1][j][0], dp[i - 1][j - 1][1]) + arr[i][j];
+                    dp[i][j][0] = dp[i - 1][j - 1][1] + arr[i][j];
+                    continue;
                 }
+
+                dp[i][j][1] = Math.min(dp[i - 1][j][0], dp[i - 1][j + 1][2]) + arr[i][j];
+                dp[i][j][0] = Math.min(dp[i - 1][j + 1][2], dp[i - 1][j - 1][1]) + arr[i][j];
+                dp[i][j][2] = Math.min(dp[i - 1][j][0], dp[i - 1][j - 1][1]) + arr[i][j];
+
             }
         }
 
         int answer = Integer.MAX_VALUE;
 
         for (int i = 0; i < m; i++) {
-            for (int j = 0; j < 3; j++) {
-                answer = Math.min(dp[n-1][i][j], answer);
-            }
+            answer = Math.min(dp[n - 1][i][0], answer);
+            answer = Math.min(dp[n - 1][i][1], answer);
+            answer = Math.min(dp[n - 1][i][2], answer);
         }
 
         sb.append(answer).append("\n");
         System.out.println(sb);
     }
 
-    static boolean isArea(int x){
-        if(x < 0 || x >= m ) return false;
-        return true;
-    }
+
 }
